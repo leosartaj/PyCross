64b43d6bb36b0fec1a467847adf1c1f4730ee9af
Sartaj Singh <singhsartaj94@gmail.com>
2014-10-03 04:38:11 +0530

    cleaner error handling

--- TicTacToe_ter.py
+++ TicTacToe_ter.py
@@ -13,33 +13,26 @@ import helper
 
 
 def main():
-    while True:
-        try:
-            diff = raw_input("Enter Difficulty level [E|M|H] --> ")
-        except:
-            return
-        if diff== 'E':
-            NTRIALS = 10  # Number of trials to run
-            break
-        elif diff== 'M':
-            NTRIALS = 25
-            break
-        elif diff== 'H':
-            NTRIALS = 100
-            break
     try:
+        while True:
+            diff = raw_input("Enter Difficulty level [E|M|H] --> ")
+            if diff== 'E':
+                NTRIALS = 10  # Number of trials to run
+                break
+            elif diff== 'M':
+                NTRIALS = 25
+                break
+            elif diff== 'H':
+                NTRIALS = 100
+                break
         dim = input("Size of grid --> ")
-    except: 
-        return
-
-    while True:
-        play.play_terminal(dim, helper.mc_move, NTRIALS) # plays the game on a terminal
-        try:
+        while True:
+            play.play_terminal(dim, helper.mc_move, NTRIALS) # plays the game on a terminal
             game = raw_input("Another game ? [Y/N] --> ")
-        except:
-            return
-        if game != 'Y' and game != '':
-            return
+            if game != 'Y' and game != '':
+                return
+    except KeyboardInterrupt: 
+        return
 
 if __name__ == '__main__':
     main()
--- play.py
+++ play.py
@@ -19,39 +19,36 @@ def play_terminal(dim, mc_move, trials):
     Makes the game playable on a terminal
     """
     board = TTTBoard.TTTBoard(dim)
-    while True:
-        try:
-            other = raw_input("You are O or X [O/X] --> ")
-        except:
-            return
-        if other == X or other == O:
-            break
-        if other == '':
-            other = O
-            break
-    player = switch_player(other)
-    while True:
-        if format_result(board, player):
-            return
-        move = mc_move(board, player, trials)
-        board.move(player, move[0], move[1])
-        if format_result(board, player):
-            return
-        print board
+    try:
         while True:
-            try:
+            other = raw_input("You are O or X [O/X] --> ")
+            if other == X or other == O:
+                break
+            if other == '':
+                other = O
+                break
+        player = switch_player(other)
+        while True:
+            if format_result(board, player):
+                return
+            move = mc_move(board, player, trials)
+            board.move(player, move[0], move[1])
+            if format_result(board, player):
+                return
+            print board
+            while True:
                 rowcol = raw_input("[row] [col] --> ")
                 rowcol = rowcol.split(' ')
                 if len(rowcol) != 2:
                     continue
                 orow = int(rowcol[0])
                 ocol = int(rowcol[1])
-            except KeyboardInterrupt:
-                return
-            if orow > -1 and orow < dim and ocol > -1 and ocol < dim and board.square(orow, ocol) == EMPTY:
-                break
-            print ''
-        board.move(other, orow, ocol)
+                if orow > -1 and orow < dim and ocol > -1 and ocol < dim and board.square(orow, ocol) == EMPTY:
+                    break
+                print ''
+            board.move(other, orow, ocol)
+    except KeyboardInterrupt: # return if interrupt recieved
+        return
 
 def format_result(board, player): # prints the result and returns True if the game is over otherwise returns False
     result = board.check_win()
