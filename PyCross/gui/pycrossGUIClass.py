cca107758ad51d87c302e1cf9b38bc21b2f6aad9
Sartaj Singh <singhsartaj94@gmail.com>
2014-11-14 01:56:40 +0530

    SVG icons

---
+++ PyCross/gui/pycrossGUIClass.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+
+##
+# PyCross
+# https://github.com/leosartaj/PyCross.git
+#
+# Copyright (c) 2014 Sartaj Singh
+# Licensed under the MIT license.
+##
+
+# system imports
+import os
+
+# For the GUI
+import gtk
+
+class pycrossGUIClass:
+    """ 
+    Sets up the GUI interface
+    """
+    def __init__(self):
+
+        self.load_interface() # load the interface
+
+        self.save_objects() # save objects
+
+        self.basic_markup() # setup appearances
+
+        self.builder.connect_signals(self.setup_signals()) # setup signals
+
+        self.window.show_all() # display widgets
+
+    def load_interface(self):
+        """
+        Loads the interface
+        in particular loads the glade file
+        """
+        fName = os.path.join(os.path.dirname(__file__), 'pycrossGUI.glade') # hack for loading the glade file
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(fName)
+
+    def setup_signals(self):
+        """
+        Sets up the signals
+        """
+        sig = { 'on_quit_activate': self.close }
+
+        return sig
+
+    def save_objects(self):
+        """
+        Get the required objects
+        """
+        self.window = self.builder.get_object('mainwindow')
+        self.board = self.builder.get_object('board')
+
+    def basic_markup(self):
+        """
+        set the appearances, 'cause appearances are good
+        """
+        pass
+
+    def close(self, *args):
+        """
+        Handles Destroy Event
+        """
+        gtk.main_quit()
---
+++ PyCross/gui/svg/iconCreator.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2
+
+##
+# PyCross
+# https://github.com/leosartaj/PyCross.git
+#
+# Copyright (c) 2014 Sartaj Singh
+# Licensed under the MIT license.
+##
+
+"""
+Creates SVG for PyCross
+Supports creating blank, cross or circle SVG
+"""
+
+from math import pi
+import optparse 
+import cairo
+
+def parse_args():
+    """
+    Parses the arguments
+    """
+    usage = """usage: %prog [type] [options]
+
+    Types:
+    blank -> for blank SVG
+    cross -> for cross SVG
+    circle -> for circle SVG
+
+    Run 
+    python2 iconCreator.py -h/--help
+    For help
+"""
+    parser = optparse.OptionParser(usage)
+
+    help = "Height of SVG"
+    parser.add_option('--height', type='int', help=help, default=100)
+
+    help = "Width of SVG"
+    parser.add_option('--width', type='int', help=help, default=100)
+
+    options, args = parser.parse_args()
+
+    if len(args) == 1:
+        element = args[0]
+    else:
+        parser.error('Invalid number of arguments, got ' + str(len(args)) + ' args expecting 1/2')
+
+    if element == 'blank' or element == 'cross' or element == 'circle':
+        return options, element
+    else:
+        parser.error('Invalid argument ' + args[0])
+
+
+def draw_blank(options, fName='blank.svg'):
+    """
+    Creates Blank SVG
+    """
+    ps  = cairo.SVGSurface(fName, options.height, options.width)
+    cr = cairo.Context(ps)
+
+def draw_cross(options, fName='cross.svg'):
+    """
+    Creates Cross SVG
+    """
+    height, width = options.height, options.width
+    ps  = cairo.SVGSurface(fName, options.height, options.width)
+    cr = cairo.Context(ps)
+    cr.set_source_rgb(0, 0, 0)
+    cr.set_line_width(10)
+    cr.move_to(0, 0)
+    cr.line_to(height, width)
+    cr.move_to(0, width)
+    cr.line_to(height, 0)
+    cr.stroke()
+
+def draw_circle(options, fName='circle.svg'):
+    """
+    Creates Circle SVG
+    """
+    height, width = options.height, options.width
+    ps  = cairo.SVGSurface(fName, options.height, options.width)
+    cr = cairo.Context(ps)
+    cr.set_source_rgb(0, 0, 0)
+    cr.set_line_width(10)
+    cr.arc(height / 2, width / 2, (height / 2) - 5, 0, 2 * pi)
+    cr.stroke()
+
+if __name__ == '__main__':
+    options, element = parse_args() # parse the args
+
+    if element == 'blank':
+        draw_blank(options)
+    elif element == 'cross':
+        draw_cross(options)
+    elif element == 'circle':
+        draw_circle(options)
